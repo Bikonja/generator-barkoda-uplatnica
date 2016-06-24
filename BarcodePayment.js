e2ffc89d858329c0823e722244ed1f2f80c8906e
Bikonja <bikonja@gmail.com>
2016-06-24 18:05:40 +0200

    Dodan razmak u listu dozvoljenih znakova

--- BarcodePayment.js
+++ BarcodePayment.js
@@ -1,12 +1,12 @@
 /**
  * BarcodeGenerator - 2D Barcode generator for Croatian payment(LGPLv3)
- * version: 0.501
+ * version: 0.502
  */
 BarcodePayment = new function() {
 	var _me = this;
 	
 	// Constants
-	var _allowedSingleByteCharacters = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ",", ".", ":", "-", "+", "?", "'", "/", "(", ")" ];
+	var _allowedSingleByteCharacters = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ",", ".", ":", "-", "+", "?", "'", "/", "(", ")" ];
 	var _allowedTwoByteCharacters = [ "Š", "Đ", "Č", "Ć", "Ž", "š", "đ", "č", "ć", "ž" ];
 	var _allowedCharacters = jQuery.merge(jQuery.merge([], _allowedSingleByteCharacters), _allowedTwoByteCharacters);
 	
